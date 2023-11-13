44b2044c8f3810caad05f6baa987c9eb1939d4fb
inkyu-yoon <tryingmybest24h@gmail.com>
2023-11-14 00:59:56 +0900

    test: prizeEventConstants method test

---
+++ src/test/java/christmas/constants/PrizeEventConstantsTest.java
@@ -0,0 +1,34 @@
+package christmas.constants;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrizeEventConstantsTest {
+
+
+    @DisplayName("증정 이벤트 조건에 맞는 금액 이상을 전달하면 증정품 정보를 반환한다.")
+    @Test
+    void successGetPrizeInfo() {
+        PrizeEventConstants prizeEventConstants = PrizeEventConstants.PRIZE;
+        int minimumPayment = prizeEventConstants.minimumPayment;
+
+        assertThat(prizeEventConstants.createPrizeInfo(minimumPayment))
+                .isEqualTo(
+                        String.format("%s %d개",
+                                prizeEventConstants.prize.getName(),
+                                prizeEventConstants.countOfPrize)
+                );
+    }
+
+    @DisplayName("증정 이벤트 조건에 맞는 금액을 만족하지 못하면 '없음'을 반환한다.")
+    @Test
+    void failGetPrizeInfo() {
+        PrizeEventConstants prizeEventConstants = PrizeEventConstants.PRIZE;
+        int minimumPayment = prizeEventConstants.minimumPayment - 1;
+
+        assertThat(prizeEventConstants.createPrizeInfo(minimumPayment))
+                .isEqualTo("없음");
+    }
+}
