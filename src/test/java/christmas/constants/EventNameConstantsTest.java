382e4b8da068b3fa1db54d5a058876f0fd2aca43
inkyu-yoon <tryingmybest24h@gmail.com>
2023-11-14 00:53:43 +0900

    test: eventNameConstants method test

---
+++ src/test/java/christmas/constants/EventNameConstantsTest.java
@@ -0,0 +1,39 @@
+package christmas.constants;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static christmas.constants.EventNameConstants.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventNameConstantsTest {
+
+    String nameOfChristmasEvent = "크리스마스 디데이 할인";
+    String nameOfPrizeEvent = "증정 이벤트";
+    String nameOfSpecialEvent = "특별 할인";
+    String nameOfWeekdayEvent = "평일 할인";
+    String nameOfWeekendEvent = "주말 할인";
+
+    private Stream<Arguments> successGetEventNameScenarios() {
+        return Stream.of(
+                Arguments.of(CHRISTMAS_EVENT, nameOfChristmasEvent),
+                Arguments.of(PRIZE_EVENT, nameOfPrizeEvent),
+                Arguments.of(SPECIAL_EVENT, nameOfSpecialEvent),
+                Arguments.of(WEEKDAY_EVENT, nameOfWeekdayEvent),
+                Arguments.of(WEEKEND_EVENT, nameOfWeekendEvent)
+        );
+    }
+
+    @DisplayName("지정된 이벤트 이름을 반환한다.")
+    @ParameterizedTest
+    @MethodSource("successGetEventNameScenarios")
+    void success(EventNameConstants eventNameConstants, String nameOfEvent) {
+        assertThat(eventNameConstants.getName()).isEqualTo(nameOfEvent);
+    }
+}
