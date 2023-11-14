d83d4e054c1864f03c3153cd6ff00c8b35567e2f
inkyu-yoon <tryingmybest24h@gmail.com>
2023-11-14 19:43:17 +0900

    test: christmasEvent method test

--- src/test/java/christmas/domain/event/ChristmasEventTest.java
+++ src/test/java/christmas/domain/event/ChristmasEventTest.java
@@ -4,11 +4,14 @@ import christmas.domain.Menu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.EnumMap;
+import java.util.stream.Stream;
 
-import static christmas.constants.EventNameConstants.CHRISTMAS_EVENT;
+import static christmas.constants.EventNameConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChristmasEventTest {
@@ -63,5 +66,38 @@ class ChristmasEventTest {
         String nameOfEvent = christmasEvent.getName();
 
         assertThat(nameOfEvent).isEqualTo(CHRISTMAS_EVENT.getName());
+    }
+
+    @DisplayName("1일부터 1000원으로 시작하여 크리스마스가 다가올 수록 할인 금액이 100원 증가한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10, 20, 25})
+    void calculateBenefit(int visitDay) {
+        Menu menuSatisfyMinimumPrice = Menu.CHOCOLATE_CAKE;
+        orderInfo.put(menuSatisfyMinimumPrice, 1);
+        int benefit = christmasEvent.calculateBenefit(orderInfo, visitDay);
+
+        assertThat(benefit).isEqualTo(900 + 100 * visitDay);
+    }
+
+    @DisplayName("26일 이후에 방문하면 할인금액이 0원이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {26, 29, 30, 31})
+    void calculateBenefitNotAppliedByVisitDay(int visitDay) {
+        Menu menuSatisfyMinimumPrice = Menu.CHOCOLATE_CAKE;
+        orderInfo.put(menuSatisfyMinimumPrice, 1);
+        int benefit = christmasEvent.calculateBenefit(orderInfo, visitDay);
+
+        assertThat(benefit).isEqualTo(0);
+    }
+
+    @DisplayName("26일 이전에 방문했지만 총 주문금액이 만원 미만이면 할인금액이 0원이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {26, 29, 30, 31})
+    void calculateBenefitNotAppliedByPayment(int visitDay) {
+        Menu menuSatisfyMinimumPrice = Menu.CAESAR_SALAD;
+        orderInfo.put(menuSatisfyMinimumPrice, 1);
+        int benefit = christmasEvent.calculateBenefit(orderInfo, visitDay);
+
+        assertThat(benefit).isEqualTo(0);
     }
 }
